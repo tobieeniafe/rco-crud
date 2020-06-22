cbc1f2658a25008ca1b9332005a18304cbbf367d
Samuel Sola-Eniafe <tobie@Tobies-MacBook-Pro.local>
2020-06-22 12:51:39 +0100

    sign up and register

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="navbar-fixed">
+        <nav className="z-depth-0">
+          <div className="nav-wrapper white">
+            <Link
+              to="/"
+              style={{
+                fontFamily: "monospace"
+              }}
+              className="col s5 brand-logo center black-text"
+            >
+              R&amp;Co CRUD APP
+            </Link>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+              <li>
+                <Link
+                  to="/register"
+                  style={{
+                    width: "140px",
+                    borderRadius: "3px",
+                    letterSpacing: "1.5px"
+                  }}
+                  className="black-text"
+                >
+                  Register
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/login"
+                  style={{
+                    width: "140px",
+                    borderRadius: "3px",
+                    letterSpacing: "1.5px"
+                  }}
+                  className="black-text"
+                >
+                  Log In
+                </Link>
+              </li> 
+            </ul>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+export default Navbar;
